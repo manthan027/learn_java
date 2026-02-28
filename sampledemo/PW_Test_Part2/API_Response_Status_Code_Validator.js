9597d3154fd60edab408cefe1a6cb6cb857eab79
manthank027 <manthanjn27@gmail.com>
2026-02-28 18:00:06 +0530

    API response status code file added

---
+++ sampledemo/PW_Test_Part2/API_Response_Status_Code_Validator.js
@@ -0,0 +1,32 @@
+// As an SDET, you receive an API response status code and need to classify it. Write a JavaScript program using a switch statement that takes an HTTP status code stored in a variable and prints the category and a QA-friendly message.
+
+let statusCode = 200;
+switch (true) {
+    case (statusCode === 200 ):
+        console.log("OK: Request successful");
+        break;
+    case (statuscode === 201):
+        console.log("Created: Resource created successfully");
+        break;
+    case (statuscode === 301):
+        console.log("WARNING - Moved Permanently: URL has changed"); 
+        break;
+    case (statusCode === 400):
+        console.log("FAIL - Bad Request: Check request payload");
+        break;
+    case (statusCode === 401):
+        console.log("FAIL - Unauthorized: Check auth token");
+        break;
+    case (statusCode === 403):
+        console.log("FAIL - Forbidden: Insufficient permissions");
+        break;
+    case (statusCode === 404):
+        console.log("FAIL - Not Found: Check endpoint URL");
+        break;
+    case (statusCode === 500):
+        console.log("FAIL - Internal Server Error: Backend issue");
+        break;
+    default:
+        console.log("UNKNOWN - Unhandled status code");
+
+}
